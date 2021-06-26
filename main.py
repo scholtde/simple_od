10752ad6d14d5d6a42caada7d3332f21c5ad8357
scholtde <dewald@rc.technology>
2021-06-26 17:54:29 +0200

    removed im processing

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ cap = cv.VideoCapture(1)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -11,10 +12,9 @@ while True:
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
     # Display the resulting frame
-    cv.imshow('frame', gray)
+    cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('q'):
         break
 # When everything done, release the capture
