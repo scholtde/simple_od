b5cb713fe39513812dee68f761b77d617410faab
scholtde <dewald@rc.technology>
2021-06-26 18:05:55 +0200

    Added models directory

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import numpy as np
 import cv2
+
+CLASSES = [
+    "background", "aeroplane", "bicycle", "bird", "boat",
+    "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+    "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+    "sofa", "train", "tvmonitor"
+]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
 cap = cv2.VideoCapture(1)
 if not cap.isOpened():
     print("Cannot open camera")
